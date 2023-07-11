f3cb574b0e42f73a2b132e20086211f8c1feb973
Asalama98 <>
2023-07-11 16:08:43 +0300

    test2

--- app/src/main/java/com/example/learningcompose/MainActivity.kt
+++ app/src/main/java/com/example/learningcompose/MainActivity.kt
@@ -106,7 +106,8 @@ fun ImageCard(
             {
 
                 Text(text = title, style = TextStyle(Color.White, fontSize = 16.sp))
-
+                //test
+                //456
             }
 
         }
